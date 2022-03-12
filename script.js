03d77c062dd1ecb6fa78fa72f7ef52ebac4d2890
TiagoRibeiro25 <92998482+TiagoRibeiro25@users.noreply.github.com>
2022-03-12 23:59:29 +0000

    Upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function add_param(param) {
+    document.querySelector("[name ='screen']").value += param;
+}
+
+function calc() {
+    math_count = document.querySelector("[name ='screen']").value;
+    document.querySelector("[name='screen']").value = eval(math_count);
+}
+
+function percentage() {
+    math_count = document.querySelector("[name = 'screen']").value + '/ 100';
+    document.querySelector("[name = 'screen']").value = eval(math_count);
+}
+
+function ac_btn() {
+    document.querySelector("[name = 'screen'").value = ''
+}
